e2be372eaf171c58b8f8590b66f8f146789b00a5
pandeysagar <pandeysagar1704@gmail.com>
2023-08-27 16:01:28 +0530

    final

---
+++ src/components/MyAttendance.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import Navbar from "./Navbar";
+import "./MyAttendance.css"
+function MyAttendance() {
+  return (
+    <div className="myattendance">
+      <div className="container">
+        <div className="row">
+          <div className="col-2">
+            <Navbar />
+          </div>
+          <div className="col percentage">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Slot</th>
+                  <th scope="col">Category</th>
+                  <th scope="col">Subject</th>
+                  <th scope="col">Faculty Name</th>
+                  <th scope="col">Hours Attended</th>
+                  <th scope="col">Hours Present</th>
+                  <th scope="col">Percentage</th>
+
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>B</td>
+                  <td>Theory</td>
+                  <td>Service Oriented Architecture</td>
+                  <td>Dr. S. Ramamoorthy (102506)</td>
+                  <td>16</td>
+                  <td>2</td>
+                  <td>87.5%</td>
+                </tr>
+                <tr>
+                  <td>D</td>
+                  <td>Theory</td>
+                  <td>Natural Language Processing</td>
+                  <td>Mrs. Madhumitha K (102963)</td>
+                  <td>13</td>
+                  <td>0</td>
+                  <td>100%</td>
+                </tr>
+                <tr>
+                  <td>G</td>
+                  <td>Theory</td>
+                  <td>Environmental Impact Assessment</td>
+                  <td>Dr.P.Purushothoman (102529)</td>
+                  <td>17</td>
+                  <td>7</td>
+                  <td>58.82</td>
+                </tr>
+                <tr>
+                  <td>LAB</td>
+                  <td>Practical</td>
+                  <td>Minor Project</td>
+                  <td>Dr.T.K.SivaKumar (100487)</td>
+                  <td>12</td>
+                  <td>2</td>
+                  <td>83.33</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MyAttendance;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDom from "react-dom";
 import App from "./App"
 import { BrowserRouter as Router, Routes, 
     Route, Redirect,Switch} from "react-router-dom";
-  import { render } from 'react-dom';
-  import { BrowserRouter} from 'react-router-dom';
-  import Dashboard from "./components/Dashboard";
-  import Subjects from "./components/Subjects";
-  ReactDom.render(
+import { render } from 'react-dom';
+import { BrowserRouter} from 'react-router-dom';
+import Dashboard from "./components/Dashboard";
+import Subjects from "./components/Subjects";
+import MyAttendance from "./components/MyAttendance";
+ReactDom.render(
     <React.StrictMode>
       <BrowserRouter>
         <Routes>
@@ -15,6 +16,7 @@ import { BrowserRouter as Router, Routes,
           </Route>
           <Route path = "/Dashboard" element = {<Dashboard />}></Route>
           <Route path = "/Subjects" element = {<Subjects />}></Route>
+          <Route path = "/MyAttendance" element = {<MyAttendance />}></Route>
         </Routes>
       </BrowserRouter>
     </React.StrictMode>,
